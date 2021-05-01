d8ab6476ff1557ecb212d435f15ed034bcd4766e
emre-23 <ygormus173@gmail.com>
2021-05-01 16:24:27 +0300

    **

---
+++ linpy/main.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+res = defaultdict(list)
+
+with open('user.txt') as infile:
+    for line in infile:
+        val = line.strip().split(":")
+        #name=val[0]
+        res[val[1]].append(line)
+        
+
+for k, v in res.items():
+    if len(v) > 1:
+        print(val[1],v)
+
